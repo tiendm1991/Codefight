bee2e1d13a7e4e658fa59015476e4b148d86bf40
TienDM <tiendm1991@gmail.com>
2018-07-30 22:40:52 +0700

    xxx

--- src/tiendm/codefight/challenge/july/MiracleOfChain.java
+++ src/tiendm/codefight/challenge/july/MiracleOfChain.java
@@ -3,9 +3,10 @@ package tiendm.codefight.challenge.july;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.List;
 
 public class MiracleOfChain {
-	String[] miracleOfChain(String[] n) {
+	String[] miracleOfChain2(String[] n) {
 		ArrayList<String> r = new ArrayList<>();
 		String first = "";
 		int idxFirst = 0;
@@ -40,6 +41,38 @@ public class MiracleOfChain {
 			}
 		}
 		return r.toArray(n);
+	}
+	
+	String[] miracleOfChain(String[] n) {
+		List<String> s = new ArrayList<>();
+		for(String str : n){
+			s.add(str);
+		}
+		List<String> result = new ArrayList<>();
+		String first = s.get(0);
+		String f = first.substring(0, 1).toLowerCase();
+		String l = first.substring(first.length()-1).toLowerCase();
+		result.add(first);
+		s.remove(0);
+		while (s.size() > 0) {
+			for(int i = 0; i < s.size(); i++){
+				String check = s.get(i);
+				String fNext = check.substring(0,1).toLowerCase();
+				String lNext = check.substring(check.length()-1).toLowerCase();
+				if(fNext.equals(l)){
+					result.add(check);
+					l = lNext;
+					s.remove(i);
+					break;
+				}else if(lNext.equals(f)){
+					result.add(0, check);
+					f = fNext;
+					s.remove(i);
+					break;
+				}
+			}
+		}
+		return result.toArray(new String[n.length]);
 	}
 	public static void main(String[] args) {
 		MiracleOfChain m = new MiracleOfChain();
