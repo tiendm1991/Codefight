7d17ac15ba4b51a968f2ec2db816b325fb4dbedb
TienDM <tiendm1991@gmail.com>
2018-08-13 19:55:53 +0700

    23132

--- src/tiendm/codefight/Util.java
+++ src/tiendm/codefight/Util.java
@@ -1,11 +1,26 @@
 package tiendm.codefight;
 
 public class Util {
-	boolean isPrime(int x){
-	    if(x == 2 || x == 3) return true;
-	    for(int i = 2; i < x/2; i++){
-	        if(x % i == 0) return false;
-	    }
-	    return true;
+	boolean isPrime(int x) {
+		if (x == 2 || x == 3)
+			return true;
+		for (int i = 2; i < x / 2; i++) {
+			if (x % i == 0)
+				return false;
+		}
+		return true;
 	}
+
+	int countSumOfTwoRepresentations(int n, int l, int r) {
+		int result = 0;
+		for (int a = l; a <= r; a++) {
+			for (int b = a; b <= n; b++) {
+				if (a + b == n) {
+					result++;
+				}
+			}
+		}
+		return result;
+	}
+
 }
--- src/tiendm/codefight/challenge/august/Tour13.java
+++ src/tiendm/codefight/challenge/august/Tour13.java
@@ -1,10 +1,72 @@
 package tiendm.codefight.challenge.august;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Tour13 {
+	int summaryProficiency(int[] a, int n, int m) {
+	    Arrays.sort(a);
+	    int sum = 0;
+	    int count = 0;
+	    for(int i = a.length-1; i >=0; i--){
+	        if(count == n) break;
+	        if(a[i] >= m) {
+	            sum += a[i];
+	            count++;
+	        }
+	    }
+	    int x = 3;
+	    List<Integer> z = new ArrayList<>();
+	    z.add(x);
+	    return sum;
+	}
+	
+	String reduceString(String inputString) {
+	    while(inputString.length() > 0 && inputString.charAt(0) == inputString.charAt(inputString.length()-1)){
+	    	if(inputString.length() == 1) return "";
+	        inputString = inputString.substring(1,inputString.length()-1);
+	    }
+	    return inputString;
+	}
+	
+	int digitSum(int n) {
+	    String s = Integer.toBinaryString(n);
+	    int c = 0;
+	    for(int i = 0; i < s.length(); i++){
+	    	if(s.charAt(i) == '1'){
+	    		c++;
+	    	}
+	    }
+	    return c;
+	}
+
+	int sumBelowBound(int bound) {
+	    int n = 1, s = 0;
+	    while(s + n < bound){
+	        s += n;
+	        n++;
+	    }
+	    return n;
+	}
+	
+	int candles(int candlesNumber, int makeNew) {
+		int count = 0, leftovers = 0;
+		while(count == 0 || leftovers > 1){
+			if(leftovers != 0){
+				candlesNumber = leftovers / makeNew;
+				leftovers = leftovers % makeNew;
+			}
+			count += candlesNumber;
+			leftovers += candlesNumber;
+		}
+		return count;
+	}
+
+
 	public static void main(String[] args) {
 		Tour13 t = new Tour13();
-		System.out.println();
+		int[] x = {4, 2, 3, 6, 2, 5, 4};
+		System.out.println(t.candles(5,2));
 	}
 }
