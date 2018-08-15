af3031e3daba72367e8addeb59c27622670a5776
TienDM <tiendm1991@gmail.com>
2018-08-15 21:05:17 +0700

    xxxx

--- src/tiendm/codefight/tournament/Tour1408.java
+++ src/tiendm/codefight/tournament/Tour1408.java
@@ -41,7 +41,8 @@ public class Tour1408 {
 	boolean equationTemplate(int[] input) {
 		Arrays.sort(input);
 		return input[0] * input[1] * input[2] == input[3] || input[0] * input[3] == input[1] * input[2]
-				|| input[0] * input[1] == input[2] * input[3] || input[0] == input[1] * input[2] * input[3];
+				|| input[0] * input[1] == input[2] * input[3] || input[0] == input[1] * input[2] * input[3]
+				|| input[0] * input[2] == input[1] * input[3];
 
 	}
 
