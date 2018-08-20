7459ba110b0a35eca24b259fd3d3d646a40484f0
TienDM <tiendm1991@gmail.com>
2018-08-21 00:40:59 +0700

    xxx

--- src/tiendm/codefight/Util.java
+++ src/tiendm/codefight/Util.java
@@ -10,27 +10,22 @@ public class Util {
 		}
 		return true;
 	}
-	
+
 	int chessKnight(String cell) {
-		  int row = Integer.parseInt("" + cell.charAt(1)),
-		      column = cell.charAt(0) - 'a' + 1;
-		  int[][] steps = {
-		      {-2, -1}, {-1, -2}, {1, -2}, {2, -1},
-		      {2, 1}, {1, 2}, {-1, 2}, {-2, 1}
-		  };
-		  int answer = 0;
+		int row = Integer.parseInt("" + cell.charAt(1)), column = cell.charAt(0) - 'a' + 1;
+		int[][] steps = { { -2, -1 }, { -1, -2 }, { 1, -2 }, { 2, -1 }, { 2, 1 }, { 1, 2 }, { -1, 2 }, { -2, 1 } };
+		int answer = 0;
 
-		  for (int i = 0; i < steps.length; i++) {
-		    int tmpRow =  row + steps[i][0];
-		    int tmpColumn = column + steps[i][1];
-		    if (tmpRow >= 1 && tmpRow <= 8 &&
-		        tmpColumn >= 1 && tmpColumn <= 8) {
-		      answer++;
-		    }
-		  }
-
-		  return answer;
+		for (int i = 0; i < steps.length; i++) {
+			int tmpRow = row + steps[i][0];
+			int tmpColumn = column + steps[i][1];
+			if (tmpRow >= 1 && tmpRow <= 8 && tmpColumn >= 1 && tmpColumn <= 8) {
+				answer++;
+			}
 		}
+
+		return answer;
+	}
 
 	int countSumOfTwoRepresentations(int n, int l, int r) {
 		int result = 0;
@@ -44,17 +39,30 @@ public class Util {
 		}
 		return result;
 	}
-	
+
 	int countSumOfTwoRepresentations2(int n, int l, int r) {
-	    int c = 0;
-	    for(int i = l; i <= r; i++){
-	        if(i <= n-i && n-i <=r) c++;
-	    }
-	    return c;
+		int c = 0;
+		for (int i = l; i <= r; i++) {
+			if (i <= n - i && n - i <= r)
+				c++;
+		}
+		return c;
+	}
+
+	int countSumOfTwoRepresentations3(int n, int l, int r) {
+		int result = 0;
+
+		for (int i = 1; i <= n - i; i++) {
+			if (l <= i && n - i <= r) {
+				result++;
+			}
+		}
+
+		return result;
 	}
 
 	public static void main(String[] args) {
 		Util u = new Util();
-		System.out.println(u.countSumOfTwoRepresentations(93,24,58));
+		System.out.println(u.countSumOfTwoRepresentations(93, 24, 58));
 	}
 }
