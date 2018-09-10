4a38231718fe95b45754e912ce78b67b18cd2b54
TienDM <tiendm1991@gmail.com>
2018-09-10 23:50:45 +0700

    zczx

--- src/tiendm/codefight/tournament/sep/Tour1009.java
+++ src/tiendm/codefight/tournament/sep/Tour1009.java
@@ -1,6 +1,17 @@
 package tiendm.codefight.tournament.sep;
 
 public class Tour1009 {
+	String angleType(int measure) {
+		if (measure < 90)
+			return "acute";
+		else if (measure == 90)
+			return "right";
+		else if (measure < 180)
+			return "obtuse";
+		else
+			return "straight";
+
+	}
 
 	public static void main(String[] args) {
 		Tour1009 t = new Tour1009();
