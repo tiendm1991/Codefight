e082f7e4acf6056b74c5b6e8d80437d98421e077
TienDM <tiendm1991@gmail.com>
2018-09-10 23:50:34 +0700

    xzc

---
+++ src/tiendm/codefight/tournament/sep/Tour1009.java
@@ -0,0 +1,11 @@
+package tiendm.codefight.tournament.sep;
+
+public class Tour1009 {
+
+	public static void main(String[] args) {
+		Tour1009 t = new Tour1009();
+		int[][] x = { { 0, 1, 2, 3 }, { 1, 2, 3, 0 }, { 1, 1, 1 } };
+		int[] p = { 7, 5, 4, 4, 8 };
+		System.out.println(t);
+	}
+}
