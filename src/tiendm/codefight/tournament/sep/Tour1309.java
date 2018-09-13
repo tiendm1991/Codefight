4d1ee2fabebff1c4a596a623d0d7184bb5fe0161
TienDM <tiendm1991@gmail.com>
2018-09-14 00:12:29 +0700

    xczxczx

---
+++ src/tiendm/codefight/tournament/sep/Tour1309.java
@@ -0,0 +1,13 @@
+package tiendm.codefight.tournament.sep;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Tour1309 {
+	public static void main(String[] args) {
+		Tour1309 t = new Tour1309();
+		int[] fromV = { 2, 1, 3, 4, 4, 4, 1};
+		int[] toV =   { 3, 2, 1, 3, 2, 1, 4};
+		System.out.println(t);
+	}
+}
