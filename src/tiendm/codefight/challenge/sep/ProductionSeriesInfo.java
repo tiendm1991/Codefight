7d98a692eea0a003813e79a8582be4d7a7b6b638
TienDM <tiendm1991@gmail.com>
2018-09-14 22:41:38 +0700

    zxczxcz

--- src/tiendm/codefight/challenge/sep/ProductionSeriesInfo.java
+++ src/tiendm/codefight/challenge/sep/ProductionSeriesInfo.java
@@ -9,9 +9,9 @@ public class ProductionSeriesInfo {
 		int sum = 0;
 		int[] recipe = { 0, recipe1, recipe2 };
 		for (int i : productionSeries) {
-			if(i == 1 || i == 2){
+			if (i == 1 || i == 2) {
 				sum += recipe[i];
-			}else{
+			} else {
 				sum += i;
 			}
 		}
@@ -20,11 +20,13 @@ public class ProductionSeriesInfo {
 		} else if (ingredients < sum) {
 			return new String[] { "Out of ingredients!", String.format("Missing %d ingredients", sum - ingredients) };
 		}
-		int x = ingredients % sum;
-		x = x/(Math.max(recipe1, recipe2));
+		int x = ingredients / sum - 1;
+		if (x == 0) {
+			return new String[] { "Ok" };
+		}
 		return new String[] { "Ok", String.format("Ingredients for %d more series", x) };
 	}
-	
+
 	public static void main(String[] args) {
 		ProductionSeriesInfo p = new ProductionSeriesInfo();
 		System.out.println(p);
