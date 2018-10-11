92f538574a3d9055ce7e4eb6e5e253eb8046e706
TienDM <tiendm1991@gmail.com>
2018-10-11 21:01:20 +0700

    CXZCZ

---
+++ src/tiendm/codefight/tournament/oct/Tour1009.java
@@ -0,0 +1,27 @@
+package tiendm.codefight.tournament.oct;
+
+import java.util.Arrays;
+
+public class Tour1009 {
+	int minimalNumberOfCoins(int[] coins, int price) {
+	    int count = 0;
+	    Arrays.sort(coins);
+	    int n = coins.length-1;
+	    while(price > 0){
+	        count += price/coins[n];
+	        price = price % coins[n];
+	        n--;
+	    }
+	    return count;
+	}
+	int circleOfNumbers(int n, int firstNumber) {
+	    return (firstNumber + (n/2) ) % n;
+	}
+
+
+	public static void main(String[] args) {
+		Tour1009 t = new Tour1009();
+		int[] toV = { 3, 2, 1, 3, 2, 1, 4 };
+		System.out.println(t);
+	}
+}
