a0f32e58d5b1e082e9058817e399357601f0d799
TienDM <tiendm1991@gmail.com>
2018-10-17 07:36:44 +0700

    132132

--- src/tiendm/codefight/tournament/oct/Tour1016.java
+++ src/tiendm/codefight/tournament/oct/Tour1016.java
@@ -1,6 +1,16 @@
 package tiendm.codefight.tournament.oct;
 
 public class Tour1016 {
+	
+	int visitsOnCircularRoad(int n, int[] visitsOrder) {
+	    int s = 1, sum = 0;
+	    for(int i = 0; i < visitsOrder.length; i++){
+	        sum += Math.min(Math.abs(visitsOrder[i] - s), 
+	                        Math.abs(n - visitsOrder[i] + s));
+	        s = visitsOrder[i];
+	    }
+	    return sum;
+	}
 
 	public static void main(String[] args) {
 		Tour1016 t = new Tour1016();
