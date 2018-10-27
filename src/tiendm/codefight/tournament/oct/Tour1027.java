7bd1425826ab353ae22eeb9d6f7914193db0029f
TienDM <tiendm1991@gmail.com>
2018-10-27 13:30:39 +0700

    đâs

--- src/tiendm/codefight/tournament/oct/Tour1027.java
+++ src/tiendm/codefight/tournament/oct/Tour1027.java
@@ -110,6 +110,144 @@ public class Tour1027 {
 		return best;
 	}
 
+	String properNounCorrection(String noun) {
+
+		class Helper {
+			boolean isLowerCase(char symbol) {
+				if ('a' <= symbol && symbol <= 'z') {
+					return true;
+				}
+				return false;
+			}
+		}
+		;
+		Helper h = new Helper();
+
+		StringBuilder result = new StringBuilder();
+
+		if (h.isLowerCase(noun.charAt(0))) {
+			result.append((char) (noun.charAt(0) - 'a' + 'A'));
+		} else {
+			result.append(noun.charAt(0));
+		}
+
+		for (int i = 1; i < noun.length(); i++) {
+			if (!h.isLowerCase(noun.charAt(i))) {
+				result.append((char) (noun.charAt(i) - 'A' + 'a'));
+			} else {
+				result.append(noun.charAt(i));
+			}
+		}
+
+		return result.toString();
+	}
+
+	boolean isIPv4Address(String inputString) {
+
+		int currentNumber = 0;
+		boolean emptyField = true;
+		int countNumbers = 0;
+
+		inputString += '.';
+
+		for (int i = 0; i < inputString.length(); i++) {
+			if (inputString.charAt(i) == '.') {
+				if (emptyField) {
+					return false;
+				}
+				countNumbers++;
+				currentNumber = 0;
+				emptyField = true;
+			} else {
+				int digit = inputString.charAt(i) - '0';
+				if (digit < 0 || digit > 9) {
+					return false;
+				}
+				emptyField = false;
+				currentNumber = currentNumber * 10 + digit;
+				if (currentNumber > 255) {
+					return false;
+				}
+			}
+		}
+		return countNumbers == 4;
+	}
+
+	String integerToStringOfFixedWidth(int number, int width) {
+
+		StringBuffer result = new StringBuffer();
+
+		for (int i = 0; i < width; i++) {
+			result.append('0');
+		}
+
+		int position = result.length() - 1;
+		while (number != 0 && position >= 0) {
+			result.replace(position, position + 1, Integer.toString(number % 10));
+			number /= 10;
+			position--;
+		}
+
+		return result.toString();
+	}
+
+	int waterTubes(int[] water, int[] flowPerMinute) {
+		int result = 0;
+
+		for (int i = 0; i < water.length; i++) {
+			int minutes = water[i] / flowPerMinute[i];
+			if (water[i] % flowPerMinute[i] != 0) {
+				minutes++;
+			}
+			if (result < minutes) {
+				result = minutes;
+			}
+		}
+		return result;
+	}
+
+	ArrayList<Double> liquidMixing(ArrayList<Integer> densities) {
+		ArrayList<Double> result = new ArrayList<>();
+		result.add((double) densities.get(0));
+		for (int i = 1; i < densities.size(); i++) {
+			for (int j = 0; j <= i; j++) {
+				if (densities.get(i) <= densities.get(j)) {
+					int tmp = densities.get(i);
+					for (int k = i; k > j; k--) {
+						densities.set(k, densities.get(k - 1));
+					}
+					densities.set(j, tmp);
+					if (i % 2 == 1) {
+						result.add((densities.get((i + 1) / 2) + densities.get(i / 2)) / 2.0);
+					} else {
+						result.add((double) densities.get(i / 2));
+					}
+					break;
+				}
+			}
+		}
+		return result;
+	}
+
+	int toAndFro(int a, int b, int t) {
+		int position = a, dx;
+
+		if (a < b) {
+			dx = 1;
+		} else {
+			dx = -1;
+		}
+
+		for (int i = 0; i < t; i++) {
+			position += dx;
+			if (position == a || position == b) {
+				dx = -1 * dx;
+			}
+		}
+
+		return position;
+	}
+
 	public static void main(String[] args) {
 		Tour1027 t = new Tour1027();
 		int[] values = { 2, 3, 4 };
