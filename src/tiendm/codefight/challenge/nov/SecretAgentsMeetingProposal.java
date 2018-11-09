9bccdb5d7d677b02cad0f40760d341c00829580b
VTI\tien.dinhmanh <tien.dinhmanh@vti.com.vn>
2018-11-09 16:08:32 +0700

    czxczxc

---
+++ src/tiendm/codefight/challenge/nov/SecretAgentsMeetingProposal.java
@@ -0,0 +1,13 @@
+package tiendm.codefight.challenge.nov;
+
+public class SecretAgentsMeetingProposal {
+  String[] secretAgentsMeetingProposal(String incomingMessage, int codeNumberDiff) {
+    String[] result = new String[2];
+    String[] messageSplit = incomingMessage.split("_");
+    
+  }
+  public static void main(String[] args) {
+    SecretAgentsMeetingProposal s = new SecretAgentsMeetingProposal();
+    System.out.println(s.secretAgentsMeetingProposal("", 1));
+  }
+}
---
+++ src/tiendm/codefight/tournament/nov/Tour1109.java
@@ -0,0 +1,16 @@
+package tiendm.codefight.tournament.nov;
+
+/**
+ * @author tien.dinhmanh
+ *
+ */
+public class Tour1109 {
+
+  
+  public static void main(String[] args) {
+    Tour1109 t = new Tour1109();
+    int[] values = {-1000, 0, -2, 0};
+    String[] s = {"*", "-", "5", "6", "7"};
+    System.out.println(t);
+  }
+}
