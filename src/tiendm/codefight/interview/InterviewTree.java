93f00e6e467681a1c5905610c49c3a80c989e6af
TienDM <tiendm1991@gmail.com>
2018-11-30 23:25:23 +0700

    xczxczxc

--- src/tiendm/codefight/interview/InterviewTree.java
+++ src/tiendm/codefight/interview/InterviewTree.java
@@ -2,6 +2,7 @@ package tiendm.codefight.interview;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class InterviewTree {
 
@@ -43,27 +44,30 @@ public class InterviewTree {
     while (true) {
       List<Tree<Integer>> lsCheck = new ArrayList<>();
       int countNull = 0;
-      for(Tree<Integer> node : lsPrev){
-        if(node != null){
-          if(node.left == null) countNull++;
-          if(node.right == null) countNull++;
+      for (Tree<Integer> node : lsPrev) {
+        if (node != null) {
+          if (node.left == null)
+            countNull++;
+          if (node.right == null)
+            countNull++;
           lsCheck.add(node.left);
           lsCheck.add(node.right);
         }
       }
-      if(lsCheck.size() == 0 || lsCheck.size() == countNull){
+      if (lsCheck.size() == 0 || lsCheck.size() == countNull) {
         return true;
       }
-      if(lsCheck.size() % 2 != 0) return false;
+      if (lsCheck.size() % 2 != 0)
+        return false;
       int i = lsCheck.size() / 2 - 1;
       int j = lsCheck.size() / 2;
-      while(i >= 0){
+      while (i >= 0) {
         Tree<Integer> l = lsCheck.get(i--);
         Tree<Integer> r = lsCheck.get(j++);
-        if(l == null && r == null) {
+        if (l == null && r == null) {
           continue;
         }
-        if((l == null && r != null) || (l != null && r == null) || !l.value.equals(r.value)){
+        if ((l == null && r != null) || (l != null && r == null) || !l.value.equals(r.value)) {
           return false;
         }
       }
@@ -71,93 +75,114 @@ public class InterviewTree {
     }
   }
 
+  // -----------betterOrderTraversal: Start
+  List<Integer> betterOrderTraversal(Tree<Integer> root) {
+    List<Tree<Integer>> preTreeList = new ArrayList<>();
+    preTraversal(root, preTreeList);
+    List<Integer> preList = preTreeList.stream().map(e -> e.value).collect(Collectors.toList());
+    List<Tree<Integer>> inTreeList = new ArrayList<>();
+    inTraversal(root, inTreeList);
+    List<Integer> inList = inTreeList.stream().map(e -> e.value).collect(Collectors.toList());
+    List<Tree<Integer>> postTreeList = new ArrayList<>();
+    postTraversal(root, postTreeList);
+    List<Integer> postList = postTreeList.stream().map(e -> e.value).collect(Collectors.toList());
+    return max(preList, inList, postList);
+  }
+
+  List<Integer> max(List<Integer> preList, List<Integer> inList, List<Integer> postList) {
+    return max(max(preList, inList), postList);
+  }
+
+  List<Integer> max(List<Integer> list1, List<Integer> list2) {
+    for (int i = 0; i < list1.size(); i++) {
+      int x1 = list1.get(i);
+      int x2 = list2.get(i);
+      if (x1 < x2)
+        return list1;
+      else if (x1 > x2)
+        return list2;
+    }
+    return list1;
+  }
+
+  void preTraversal(Tree<Integer> cur, List<Tree<Integer>> preTreeList) {
+    if (cur != null) {
+      preTreeList.add(cur);
+    }
+    if (cur.left != null) {
+      preTraversal(cur.left, preTreeList);
+    }
+    if (cur.right != null) {
+      preTraversal(cur.right, preTreeList);
+    }
+  }
+
+  void inTraversal(Tree<Integer> cur, List<Tree<Integer>> inTreeList) {
+    if (cur.left == null && cur.right == null) {
+      inTreeList.add(cur);
+      return;
+    }
+    if (cur.left != null) {
+      inTraversal(cur.left, inTreeList);
+    }
+    inTreeList.add(cur);
+    if (cur.right != null) {
+      inTraversal(cur.right, inTreeList);
+    }
+  }
+
+  void postTraversal(Tree<Integer> cur, List<Tree<Integer>> postTreeList) {
+    if (cur.left == null && cur.right == null) {
+      postTreeList.add(cur);
+      return;
+    }
+    if (cur.left != null) {
+      postTraversal(cur.left, postTreeList);
+    }
+    if (cur.right != null) {
+      postTraversal(cur.right, postTreeList);
+    }
+    postTreeList.add(cur);
+  }
+  // -----------betterOrderTraversal: End
+
   public static void main(String[] args) {
     InterviewTree i = new InterviewTree();
-    Tree<Integer> t0 = new Tree<Integer>(-883);
-    Tree<Integer> t1 = new Tree<Integer>(-382);
-    Tree<Integer> t2 = new Tree<Integer>(-382);
+    Tree<Integer> t0 = new Tree<Integer>(-73);
+    Tree<Integer> t1 = new Tree<Integer>(96);
+    Tree<Integer> t2 = new Tree<Integer>(1);
+    Tree<Integer> t3 = new Tree<Integer>(25);
+    Tree<Integer> t4 = new Tree<Integer>(48);
+    Tree<Integer> t5 = new Tree<Integer>(-31);
+    Tree<Integer> t6 = new Tree<Integer>(-87);
+    Tree<Integer> t7 = new Tree<Integer>(67);
+    Tree<Integer> t8 = new Tree<Integer>(93);
+    Tree<Integer> t9 = new Tree<Integer>(98);
+    Tree<Integer> t10 = new Tree<Integer>(17);
+    Tree<Integer> t11 = new Tree<Integer>(88);
+    Tree<Integer> t12 = new Tree<Integer>(95);
+    Tree<Integer> t13 = new Tree<Integer>(-46);
+    Tree<Integer> t14 = new Tree<Integer>(-37);
+    Tree<Integer> t15 = new Tree<Integer>(4);
+    Tree<Integer> t16 = new Tree<Integer>(53);
+    Tree<Integer> t17 = new Tree<Integer>(47);
     t0.left = t1;
-    t0.right = t2;
-    Tree<Integer> t3 = new Tree<Integer>(229);
-    Tree<Integer> t4 = new Tree<Integer>(493);
-    Tree<Integer> t5 = new Tree<Integer>(493);
-    Tree<Integer> t6 = new Tree<Integer>(229);
-    t1.left = t3;
-    t1.right = t4;
-    t2.left = t5;
-    t2.right = t6;
-    Tree<Integer> t7 = new Tree<Integer>(-835);
-    Tree<Integer> t8 = new Tree<Integer>(-793);
-    Tree<Integer> t9 = new Tree<Integer>(424);
-    Tree<Integer> t10 = new Tree<Integer>(93);
-    Tree<Integer> t11 = new Tree<Integer>(93);
-    Tree<Integer> t12 = new Tree<Integer>(424);
-    Tree<Integer> t13 = new Tree<Integer>(-793);
-    Tree<Integer> t14 = new Tree<Integer>(-835);
-    t3.left = t7;
-    t3.right = t8;
-    t4.left = t9;
-    t4.right = t10;
-    t5.left = t11;
-    t5.right = t12;
-    t6.left = t13;
-    t6.right = t14;
-    Tree<Integer> t15 = new Tree<Integer>(-702);
-    Tree<Integer> t16 = new Tree<Integer>(609);
-    Tree<Integer> t17 = new Tree<Integer>(889);
-    Tree<Integer> t18 = new Tree<Integer>(-724);
-    Tree<Integer> t19 = new Tree<Integer>(212);
-    Tree<Integer> t20 = new Tree<Integer>(212);
-    Tree<Integer> t21 = new Tree<Integer>(-724);
-    Tree<Integer> t22 = new Tree<Integer>(889);
-    Tree<Integer> t23 = new Tree<Integer>(609);
-    Tree<Integer> t24 = new Tree<Integer>(-702);
-    t7.left = t15;
-    t7.right = t16;
-    t8.left = t17;
-    t10.left = t18;
-    t10.right = t19;
-    t11.left = t20;
-    t11.right = t21;
-    t13.right = t22;
-    t14.left = t23;
-    t14.right = t24;
-    Tree<Integer> t25 = new Tree<Integer>(800);
-    Tree<Integer> t26 = new Tree<Integer>(1000);
-    Tree<Integer> t27 = new Tree<Integer>(-261);
-    Tree<Integer> t28 = new Tree<Integer>(-967);
-    Tree<Integer> t29 = new Tree<Integer>(-367);
-    Tree<Integer> t30 = new Tree<Integer>(748);
-    Tree<Integer> t31 = new Tree<Integer>(-870);
-    Tree<Integer> t32 = new Tree<Integer>(-12);
-    Tree<Integer> t33 = new Tree<Integer>(-482);
-    Tree<Integer> t34 = new Tree<Integer>(-482);
-    Tree<Integer> t35 = new Tree<Integer>(-12);
-    Tree<Integer> t36 = new Tree<Integer>(-870);
-    Tree<Integer> t37 = new Tree<Integer>(748);
-    Tree<Integer> t38 = new Tree<Integer>(-367);
-    Tree<Integer> t39 = new Tree<Integer>(-966);
-    Tree<Integer> t40 = new Tree<Integer>(-261);
-    Tree<Integer> t41 = new Tree<Integer>(1000);
-    Tree<Integer> t42 = new Tree<Integer>(800);
-    t15.left = t25;
-    t15.right = t26;
-    t16.left = t27;
-    t16.right = t28;
-    t17.left = t29;
-    t17.right = t30;
-    t18.left = t31;
-    t18.right = t32;
-    t19.left = t33;
-    t20.right = t34;
-    t21.left = t35;
-    t21.right = t36;
-    t22.left = t37;
-    t22.right = t38;
-    t23.left = t39;
-    t23.right = t40;
-    t24.left = t41;
-    t24.right = t42;
-    System.out.println(i.isTreeSymmetric(t0));
+    t1.left = t2;
+    t2.left = t3;
+    t2.right = t5;
+    t3.left = t4;
+    t4.left = t6;
+    t6.left = t7;
+    t6.right = t8;
+    t7.left = t9;
+    t8.right = t10;
+    t9.left = t11;
+    t9.right = t12;
+    t10.left = t13;
+    t11.right = t14;
+    t12.left = t15;
+    t12.right = t16;
+    t14.left = t17;
+    System.out.println(i.betterOrderTraversal(t0));
   }
 }
