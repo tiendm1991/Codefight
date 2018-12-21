962c6cee17bec0c7389e48aba9f206f02947b41d
VTI\tien.dinhmanh <tien.dinhmanh@vti.com.vn>
2018-12-21 17:45:25 +0700

    czxczxczxc

--- src/tiendm/codefight/interview/InterviewBacktracking.java
+++ src/tiendm/codefight/interview/InterviewBacktracking.java
@@ -11,8 +11,32 @@ public class InterviewBacktracking {
       result.add(new ArrayList<>());
       return result;
     }
-
+    ArrayList<Integer> first = new ArrayList<>();
+    climbingBacktrack(first, result, n, k);
     return result;
+  }
+
+  void climbingBacktrack(ArrayList<Integer> cur, List<ArrayList<Integer>> result, int n, int k) {
+    for (int i = 1; i <= k; i++) {
+      int s = sum(cur);
+      if (s + i > n) {
+        break;
+      } else {
+        cur.add(i);
+        if (s + i == n) {
+          result.add(cur);
+        }
+      }
+    }
+    
+  }
+
+  int sum(ArrayList<Integer> list) {
+    int s = 0;
+    for (int x : list) {
+      s += x;
+    }
+    return s;
   }
 
   public static void main(String[] args) {
