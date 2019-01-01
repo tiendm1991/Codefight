284608ca26ef1933472415ab369aecd02eeac57b
TienDM <tiendm1991@gmail.com>
2019-01-01 21:11:54 +0700

    xczxczxc

--- src/tiendm/codefight/interview/InterviewDynamicPrograming.java
+++ src/tiendm/codefight/interview/InterviewDynamicPrograming.java
@@ -2,6 +2,9 @@ package tiendm.codefight.interview;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class InterviewDynamicPrograming {
 
@@ -76,17 +79,84 @@ public class InterviewDynamicPrograming {
         strCur = cur + "";
         single = true;
         if (i == nums.length - 1) {
-          result.add(cur+"");
+          result.add(cur + "");
         }
       }
     }
     return result;
   }
 
+  int mapDecoding(String message) {
+    String[] arr = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15",
+        "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26"};
+    List<String> legal = Stream.of(arr).map(e -> e).collect(Collectors.toList());
+    if(message.length() == 1 && !legal.contains(message)){
+      return 0;
+    }
+    if (message.length() == 0 || (message.length() == 1 && legal.contains(message))) {
+      return 1;
+    }
+    int MAX = (int) (Math.pow(10, 9) + 7);
+    char idx0 = message.charAt(message.length() - 1);
+    char idx1 = message.charAt(message.length() - 2);
+    int x0 = legal.contains(idx0 + "") ? 1 : 0;
+    int x1 = idx1 == '0' ? 0 : legal.contains(idx1 + "" + idx0) ? 1 + x0 : x0;
+    int x2 = x1;
+    int i = message.length() - 3;
+    while (i >= 0) {
+      idx0 = message.charAt(i);
+      idx1 = message.charAt(i + 1);
+      if (idx0 == '0') {
+        x2 = 0;
+        x0 = x1;
+        x1 = x2;
+      } else if (legal.contains(idx0 + "" + idx1)) {
+        x2 = (x1 + x0) % MAX;
+        x0 = x1;
+        x1 = x2;
+      } else {
+        x2 = x1;
+        x0 = x1;
+        x1 = x2;
+      }
+      i--;
+    }
+    return x2;
+  }
+  //----mapDecoding revursive
+  private int map(String message, List<String> legal, Map<String, Integer> cache, final int MAX) {
+    if (message.length() == 0 || (message.length() == 1 && legal.contains(message))) {
+      cache.put(message, 1);
+      return 1;
+    }
+    if (message.startsWith("0")) {
+      cache.put(message, 0);
+      return 0;
+    }
+    Integer value = cache.get(message);
+    if (value != null) {
+      return value;
+    }
+    String s = message.substring(0, 2);
+    String s1 = message.substring(1);
+    String s2 = message.substring(2);
+    if (legal.contains(s)) {
+      Integer x2 = map(s2, legal, cache, MAX) % MAX;
+      Integer x1 = map(s1, legal, cache, MAX) % MAX;
+      cache.put(message, (x1 + x2) % MAX);
+      return (x1 + x2) % MAX;
+    } else {
+      Integer x1 = map(s1, legal, cache, MAX) % MAX;
+      cache.put(message, x1);
+      return x1 % MAX;
+    }
+  }
 
   public static void main(String[] args) {
     InterviewDynamicPrograming i = new InterviewDynamicPrograming();
-    int[] x = {0,1};
-    System.out.println(i.composeRanges(x));// 10
+    int[] x = {0, 1};
+    System.out.println(i.mapDecoding(
+        "1221112111122221211221221212212212111221222212122221222112122212121212221212122221211112212212211211"));
   }
+
 }
