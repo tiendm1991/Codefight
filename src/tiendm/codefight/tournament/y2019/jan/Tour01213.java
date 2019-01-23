40cd7bbd66bf09e74f58336a78f9f35a92bc8702
TienDM <tiendm1991@gmail.com>
2019-01-24 01:42:25 +0700

    ZXXZ

--- src/tiendm/codefight/tournament/y2019/jan/Tour01213.java
+++ src/tiendm/codefight/tournament/y2019/jan/Tour01213.java
@@ -86,6 +86,33 @@ public class Tour01213 {
     return ls;
   }
 
+  int[] robotPath(String instructions, int bound) {
+
+    int[] dx = {-1, 0, 1, 0};
+    int[] dy = {0, 1, 0, -1};
+    int x = 0, y = 0;
+
+    String directions = "LURD";
+
+    for (int i = 0; i < instructions.length(); i++) {
+      int dirIndex = 0;
+      for (int j = 1; j < 4; j++) {
+        if (instructions.charAt(i) == directions.charAt(j)) {
+          dirIndex = j;
+        }
+      }
+      if (Math.abs(x + dx[dirIndex]) <= bound && Math.abs(y + dy[dirIndex]) <= bound) {
+        x += dx[dirIndex];
+        y += dy[dirIndex];
+      }
+    }
+
+    int[] res = new int[2];
+    res[0] = x;
+    res[1] = y;
+    return res;
+  }
+
   public static void main(String[] args) {
     Tour01213 t = new Tour01213();
     System.out.println(t.kStepMaximization(0, 3));
