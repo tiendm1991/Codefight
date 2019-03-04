314e2b0f48737a89adfa9b6b226f68f8c6b1441d
VTI\tien.dinhmanh <tien.dinhmanh@vti.com.vn>
2019-03-04 16:00:32 +0700

    abc

--- src/tiendm/codefight/challenge/y2019/feb/FillShortestPaths.java
+++ src/tiendm/codefight/challenge/y2019/feb/FillShortestPaths.java
@@ -21,24 +21,39 @@ public class FillShortestPaths {
       }
     }
     int x1 = rs;
-    int x2 = R - rs -1;
+    int x2 = R - rs - 1;
     int x3 = cs;
-    int x4 = C - cs -1;
+    int x4 = C - cs - 1;
     int min = Math.min(x1, Math.min(x2, Math.min(x3, x4)));
+    if (x1 == min) {
+      for (int i = 0; i < x1; i++) {
+        plan[i][cs] = '#';
+      }
+    }
+    if (x2 == min) {
+      for (int i = x2; i < R; i++) {
+        plan[i][cs] = '#';
+      }
+    }
+    if (x3 == min) {
+      for (int i = 0; i < x3; i++) {
+        plan[rs][i] = '#';
+      }
+    }
+    if (x4 == min) {
+      for (int i = x4; i < C; i++) {
+        plan[rs][i] = '#';
+      }
+    }
     return plan;
   }
-  
+
   public static void main(String[] args) {
     FillShortestPaths b = new FillShortestPaths();
-    char[][] c = {
-        {' ', ' ', ' ', ' ', ' ', ' '}, 
-        {' ', ' ', ' ', ' ', ' ', ' '},
-        {' ', ' ', ' ', ' ', ' ', ' '}, 
-        {' ', ' ', ' ', ' ', ' ', ' '},
-        {' ', ' ', 's', ' ', ' ', ' '}, 
-        {' ', ' ', ' ', ' ', ' ', ' '},
-        {' ', ' ', ' ', ' ', ' ', ' '}, 
-        {' ', ' ', ' ', ' ', ' ', ' '},};
+    char[][] c = {{' ', ' ', ' ', ' ', ' ', ' '}, {' ', ' ', ' ', ' ', ' ', ' '},
+        {' ', ' ', ' ', ' ', ' ', ' '}, {' ', ' ', ' ', ' ', ' ', ' '},
+        {' ', ' ', 's', ' ', ' ', ' '}, {' ', ' ', ' ', ' ', ' ', ' '},
+        {' ', ' ', ' ', ' ', ' ', ' '}, {' ', ' ', ' ', ' ', ' ', ' '},};
     System.out.println(b.fillShortestPaths(c));
   }
 }
