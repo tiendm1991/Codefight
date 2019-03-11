8fde98710b21ecae985570fe542ba8bdabdd4cdd
VTI\tien.dinhmanh <tien.dinhmanh@vti.com.vn>
2019-03-11 15:31:39 +0700

    czxcz

--- src/tiendm/codefight/challenge/y2019/mar/SumInRange.java
+++ src/tiendm/codefight/challenge/y2019/mar/SumInRange.java
@@ -1,5 +1,8 @@
 package tiendm.codefight.challenge.y2019.mar;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class SumInRange {
   int sumInRange(int[] nums, int[][] queries) {
     int mod = (int) (Math.pow(10, 9) + 7);
@@ -14,5 +17,23 @@ public class SumInRange {
       s = (s + count[i] * nums[i]) % mod;
     }
     return (s + mod) % mod;
+  }
+
+  int sumInRange2(int[] nums, int[][] queries) {
+    int mod = (int) (Math.pow(10, 9) + 7);
+    int sum = 0;
+    Map<String, Integer> map = new HashMap<>();
+    for (int i = 0; i < nums.length; i++) {
+      int s = 0;
+      for (int j = i; j < nums.length; j++) {
+        s = (s + nums[j]) % mod;
+        map.put(i + "-" + j, s);
+      }
+    }
+    for (int[] q : queries) {
+      String key = q[0] + "-" + q[1];
+      sum = (sum + map.get(key)) % mod;
+    }
+    return (sum + mod) % mod;
   }
 }
